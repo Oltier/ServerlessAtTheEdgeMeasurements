a18d3fdc150203d64e705d8f2d957f31cdad8a7b
Zoltan Tudlik <zoltan@smartly.io>
2019-05-11 20:46:42 +0300

    basic getting data from event

--- Greengrass/ObjectClassificationByImage/main_on_demand.py
+++ Greengrass/ObjectClassificationByImage/main_on_demand.py
@@ -5,27 +5,28 @@ import greengrasssdk
 
 import load_model
 
-client = greengrasssdk.client('iod-data')
+client = greengrasssdk.client('iot-data')
 
-model_path = '/greengrass-machine-learning/mxnet/squeezenet/'
-model = load_model.Model(model_path + 'synset.txt', model_path + 'squeezenet_v1.1')
+
+# model_path = '/greengrass-machine-learning/mxnet/squeezenet/'
+# model = load_model.Model(model_path + 'synset.txt', model_path + 'squeezenet_v1.1')
 
 
 def object_classification_run(image):
-    if model is not None:
-        try:
-            # predictions = model.predict_from_image(image)
-            # payload = 'Prediction: {}'.format(str(predictions))
-            client.publish(topic='object_classification/response', payload='Hello lambda!')
-        except:
-            e = sys.exc_info()[0]
-            print("Exception occured during prediction: %s" % e)
-            exctype, value, tb = sys.exc_info()
-            traceback.print_tb(tb)
+    # if model is not None:
+    try:
+        # predictions = model.predict_from_image(image)
+        # payload = 'Prediction: {}'.format(str(predictions))
+        client.publish(topic='object_classification/response', payload='Hello lambda!')
+    except:
+        e = sys.exc_info()[0]
+        print("Exception occured during prediction: %s" % e)
+        exctype, value, tb = sys.exc_info()
+        traceback.print_tb(tb)
 
 
 def function_handler(event, context):
     object_classification_run('')
+    # Event contains the payload
     print(event)
-    print(context)
     return
