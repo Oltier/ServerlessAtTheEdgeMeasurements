e2545755674cff9f486e6bff095753932bf33fcc
Zoltan Tudlik <zoltan@smartly.io>
2019-05-11 23:53:10 +0300

    implement image sending with base64

--- Greengrass/ObjectClassificationByImage/load_model.py
+++ Greengrass/ObjectClassificationByImage/load_model.py
@@ -14,17 +14,20 @@ class Model(object):
 
         symbol, arg_params, aux_params = mx.model.load_checkpoint(network_prefix, 0)
 
-        self.module = mx.mod.Module(symbol, label_names=['prob_label'], context=mx.cpu())
-        self.module.bind([('data', (1, 3, 224, 224))], for_training=False)
+        self.module = mx.mod.Module(symbol=symbol, label_names=['prob_label'], context=mx.cpu())
+        self.module.bind(data_shapes=[('data', (1, 3, 224, 224))], for_training=False)
         self.module.set_params(arg_params, aux_params)
 
-    def predict_from_image(self, image):
+    def predict_from_image(self, img_str):
         reshape = (224, 224)
+
+        nparr = np.fromstring(img_str, np.uint8)
+        image = cv2.imdecode(nparr, cv2.CV_LOAD_IMAGE_COLOR)
 
         img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
         if img is None:
-            return 'unkown'
+            return 'unknown'
 
         img = cv2.resize(img, reshape)
         img = np.swapaxes(img, 0, 2)
--- Greengrass/ObjectClassificationByImage/main_on_demand.py
+++ Greengrass/ObjectClassificationByImage/main_on_demand.py
@@ -1,32 +1,48 @@
 import sys
-import datetime
+import timeit
 import traceback
 import greengrasssdk
+import json
+import time
+import base64
+import numpy as np
+import cv2
 
 import load_model
 
 client = greengrasssdk.client('iot-data')
 
 
-# model_path = '/greengrass-machine-learning/mxnet/squeezenet/'
-# model = load_model.Model(model_path + 'synset.txt', model_path + 'squeezenet_v1.1')
+model_path = '/greengrass-machine-learning/mxnet/squeezenet/'
+model = load_model.Model(model_path + 'synset.txt', model_path + 'squeezenet_v1.1')
 
 
-def object_classification_run(image):
-    # if model is not None:
-    try:
-        # predictions = model.predict_from_image(image)
-        # payload = 'Prediction: {}'.format(str(predictions))
-        client.publish(topic='object_classification/response', payload='Hello lambda!')
-    except:
-        e = sys.exc_info()[0]
-        print("Exception occured during prediction: %s" % e)
-        exctype, value, tb = sys.exc_info()
-        traceback.print_tb(tb)
+def object_classification_run(input_payload):
+    if model is not None:
+        try:
+            processing_start_time = timeit.default_timer()
+            original_message = input_payload['message']
+            image_string = base64.b64decode(original_message)
+            predictions = model.predict_from_image(image_string)
+            time.sleep(1)
+            processing_end_time = timeit.default_timer()
+            message = {
+                'processing_start_time': processing_start_time,
+                'processing_end_time': processing_end_time,
+                'prediction': str(predictions)
+            }
+            payload = json.dumps(message)
+            client.publish(topic='object_classification/response', payload=payload)
+        except:
+            e = sys.exc_info()[0]
+            print("Exception occured during prediction: %s" % e)
+            exctype, value, tb = sys.exc_info()
+            traceback.print_tb(tb)
+    else:
+        print('Model is None')
 
 
 def function_handler(event, context):
-    object_classification_run('')
+    object_classification_run(event)
     # Event contains the payload
-    print(event)
     return
