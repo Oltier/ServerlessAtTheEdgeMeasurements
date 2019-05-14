84c19d4099b4d61298574c28857f98e14ce10e06
Zoltan Tudlik <zoltan@smartly.io>
2019-05-14 16:40:19 +0300

    sending image between device and exge works

--- Azure/ObjectClassificationByImage/modules/ObjectClassificationByImage/main_on_demand.py
+++ Azure/ObjectClassificationByImage/modules/ObjectClassificationByImage/main_on_demand.py
@@ -1,9 +1,17 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for
+# full license information.
+
 import sys
 import time
 import timeit
 import traceback
 import json
 import base64
+import json
+import random
+import time
+import sys
 import iothub_client
 # pylint: disable=E0611
 from iothub_client import IoTHubModuleClient, IoTHubClientError, IoTHubTransportProvider
@@ -14,19 +22,34 @@ import load_model
 model_path = './squeezenet/'
 model = load_model.Model(model_path + 'synset.txt', model_path + 'squeezenet_v1.1')
 
+# messageTimeout - the maximum time in milliseconds until a message times out.
+# The timeout period starts at IoTHubModuleClient.send_event_async.
+# By default, messages do not expire.
 MESSAGE_TIMEOUT = 10000
-OUTPUT_QUEUE_NAME = 'object_classification/response'
-INPUT_QUEUE_NAME = 'object_classification/request'
+
+# global counters
+RECEIVE_CALLBACKS = 0
+SEND_CALLBACKS = 0
+TEMPERATURE_THRESHOLD = 25
+TWIN_CALLBACKS = 0
+
+# Choose HTTP, AMQP or MQTT as transport protocol.  Currently only MQTT is supported.
+PROTOCOL = IoTHubTransportProvider.MQTT
 
 
-def receive_message_callback(message, hub_manager):
+# receive_message_callback is invoked when an incoming message arrives on the specified 
+# input queue (in the case of this sample, "input1").  Because this is a filter module, 
+# we forward this message to the "output1" queue.
+def receive_message_callback(message, hubManager):
+    global RECEIVE_CALLBACKS
+    global TEMPERATURE_THRESHOLD
     message_buffer = message.get_bytearray()
     message_size = len(message_buffer)
     input_payload_json = message_buffer[:message_size].decode('utf-8')
     input_payload = json.loads(input_payload_json)
     print(input_payload)
-    #eper
-    # response_payload = object_classification_run(input_payload)
+    response_payload = object_classification_run(input_payload)
+    print(response_payload)
     # hub_manager.respond(response_payload)
     return IoTHubMessageDispositionResult.ACCEPTED
 
@@ -59,8 +82,9 @@ def object_classification_run(input_payload):
 
 class HubManager(object):
 
-    def __init__(self):
-        protocol = IoTHubTransportProvider.MQTT
+    def __init__(
+            self,
+            protocol=IoTHubTransportProvider.MQTT):
         self.client_protocol = protocol
         self.client = IoTHubModuleClient()
         self.client.create_from_environment(protocol)
@@ -68,17 +92,17 @@ class HubManager(object):
         # set the time until a message times out
         self.client.set_option("messageTimeout", MESSAGE_TIMEOUT)
 
-        # sets the callback when a message arrives on "input1" queue.  Messages sent to
+        # sets the callback when a message arrives on "input1" queue.  Messages sent to 
         # other inputs or to the default will be silently discarded.
-        self.client.set_message_callback(INPUT_QUEUE_NAME, receive_message_callback, self)
-
-    def respond(self, response_payload):
-        self.client.send_event_async(OUTPUT_QUEUE_NAME, response_payload, None, None)
+        self.client.set_message_callback("input1", receive_message_callback, self)
 
 
-def main():
+def main(protocol):
     try:
-        hub_manager = HubManager()
+        print ("\nPython %s\n" % sys.version)
+        print ("IoT Hub Client for Python")
+
+        hub_manager = HubManager(protocol)
 
         print ("Starting the IoT Hub Python sample using protocol %s..." % hub_manager.client_protocol)
         print ("The sample is now waiting for messages and will indefinitely.  Press Ctrl-C to exit. ")
@@ -94,4 +118,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    main(PROTOCOL)
