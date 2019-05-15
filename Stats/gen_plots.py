0aa52509cd89495243630c68c51ff4a084e9c15f
Zoltan Tudlik <zoltan@smartly.io>
2019-05-15 20:01:19 +0300

    dirty

--- Stats/gen_plots.py
+++ Stats/gen_plots.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from scipy import stats
 
 
 def calc_processing_delay(row):
@@ -26,18 +27,31 @@ df_aws_stats = pd.read_csv('AWS_stats.csv', delimiter=',')
 df_azure_stats['processing_delay'] = df_azure_stats.apply(calc_processing_delay, axis=1)
 df_azure_stats['network_delay'] = df_azure_stats.apply(calc_network_delay_azure, axis=1)
 df_azure_stats['overall_delay'] = df_azure_stats.apply(calc_overall_delay, axis=1)
+k2, p = stats.normaltest(df_azure_stats['overall_delay'])
+alpha = 1e-3
 
 print("Azure overall delay mean: {}".format(df_azure_stats['overall_delay'].mean()))
+print("Azure overall delay std: {}".format(df_azure_stats['overall_delay'].std()))
+print("Azure overall delay normaltest p: {}".format(p))
+if p < alpha:
+    print("The null hypothesis can be rejected")
+else:
+    print("The null hypothesis cannot be rejected")
 print("Azure network delay mean: {}".format(df_azure_stats['network_delay'].mean()))
+print("Azure network delay std: {}".format(df_azure_stats['network_delay'].std()))
 print("Azure processing delay mean: {}".format(df_azure_stats['processing_delay'].mean()))
+print("Azure processing delay std: {}".format(df_azure_stats['processing_delay'].std()))
 
 df_aws_stats['processing_delay'] = df_aws_stats.apply(calc_processing_delay, axis=1)
 df_aws_stats['network_delay'] = df_aws_stats.apply(calc_network_delay_aws, axis=1)
 df_aws_stats['overall_delay'] = df_aws_stats.apply(calc_overall_delay, axis=1)
 
 print("AWS overall delay mean: {}".format(df_aws_stats['overall_delay'].mean()))
+print("AWS overall delay std: {}".format(df_aws_stats['overall_delay'].std()))
 print("AWS network delay mean: {}".format(df_aws_stats['network_delay'].mean()))
+print("AWS network delay std: {}".format(df_aws_stats['network_delay'].std()))
 print("AWS processing delay mean: {}".format(df_aws_stats['processing_delay'].mean()))
+print("AWS processing delay std: {}".format(df_aws_stats['processing_delay'].std()))
 
 overall_delay = pd.DataFrame({
     'overall_delay': np.concatenate((df_aws_stats['overall_delay'].values, df_azure_stats['overall_delay'].values)),
